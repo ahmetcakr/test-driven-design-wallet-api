ee14c2a25dd5d77352fa280d67d0b3c5ea067fc8
Ahmet ÇAKIR <89451731+ahmetcakr@users.noreply.github.com>
2026-06-29 21:26:09 +0300

    chore: initial project setup with spring boot

---
+++ src/main/java/cakir/test_driven_design_wallet_api/TestDrivenDesignWalletApiApplication.java
@@ -0,0 +1,13 @@
+package cakir.test_driven_design_wallet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDrivenDesignWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDrivenDesignWalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cakir/test_driven_design_wallet_api/TestDrivenDesignWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cakir.test_driven_design_wallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDrivenDesignWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
